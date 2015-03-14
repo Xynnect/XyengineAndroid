6a9dfb3f32bd16ce6c7fe624e1cf27f7c23527d3
Dian Yordanov <dido.jordanov7@gmail.com>
2015-03-14 18:22:48 +0000

    Xyengine with gradle and android studio

---
+++ app/src/main/java/com/xyengine/xelnectmobileuser/xyengineandroid/xye.java
@@ -0,0 +1,30 @@
+package com.xyengine.xelnectmobileuser.xyengineandroid;
+
+/**
+ * Created by XelnectMobileUser on 3/14/2015.
+ */
+public class xye {
+// For the swarm!!!
+//xyengine as it is now can be used for methods but it is not to be used for other types of information
+
+
+        public static void helloWorldExample(){
+            System.out.print("hello world");
+        }
+
+        public static void helloWorldExample1(){
+            System.out.print("hello world");
+        }
+
+        public static void helloWorldExample2(){
+            System.out.print("hello world");
+        }
+        /**
+         *javadoc test
+         */
+
+        public static void helloWorldExample(String stringToBeAppended){
+            System.out.print("hello world" + stringToBeAppended);
+        }
+}
+
